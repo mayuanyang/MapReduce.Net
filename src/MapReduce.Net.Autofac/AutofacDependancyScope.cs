a7bcf824eede41549400b46535a2955d83370232
Eddy Ma <eddy.ma@smsmt.com>
2017-04-09 14:53:52 +1000

    Add AutofacDependancyScope

---
+++ src/MapReduce.Net.Autofac/AutofacDependancyScope.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Autofac;
+
+namespace MapReduce.Net.Autofac
+{
+    public class AutofacDependancyScope : IDependancyScope
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public AutofacDependancyScope(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public void Dispose()
+        {
+            _lifetimeScope.Dispose();
+        }
+
+        public T Resolve<T>()
+        {
+            return _lifetimeScope.Resolve<T>();
+        }
+
+        public object Resolve(Type t)
+        {
+            return _lifetimeScope.Resolve(t);
+        }
+    }
+}
